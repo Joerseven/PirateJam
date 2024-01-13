6d0df1814429c1938cf44ddd5a185c9791522f56
Joseph Hydon <j.d.hydon@gmail.com>
2024-01-13 20:03:38 +0000

    clean player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     private PlayerInput playerInput;
     private PlayerControls playerControls;
 
+    private Vector2 inputValue;
+
     [SerializeField] private float movementSpeed = 10.0f;
 
     private void Awake()
@@ -19,19 +21,16 @@ public class PlayerMovement : MonoBehaviour
 
         playerControls = new PlayerControls();
         playerControls.Player.Enable();
-        playerControls.Player.Move.performed += Move_performed;
     }
-
-    private void Move_performed(InputAction.CallbackContext context)
+    
+    private void Update()
     {
-        Vector2 inputValue = context.ReadValue<Vector2>();
-        rb.AddForce(inputValue * (movementSpeed * Time.fixedDeltaTime), ForceMode2D.Impulse);
+        inputValue = playerControls.Player.Move.ReadValue<Vector2>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 inputValue = playerControls.Player.Move.ReadValue<Vector2>();
         rb.AddForce(inputValue * (movementSpeed * Time.fixedDeltaTime), ForceMode2D.Impulse);
     }
 }
