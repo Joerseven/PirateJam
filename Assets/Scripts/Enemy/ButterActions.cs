7085cc214576e7bd9f18593ab275e79983c2dd1a
Joseph Hydon <j.d.hydon@gmail.com>
2024-01-19 16:31:21 +0000

    Add butter charging

--- Assets/Scripts/Enemy/ButterActions.cs
+++ Assets/Scripts/Enemy/ButterActions.cs
@@ -13,29 +13,84 @@ public class ButterActions : MonoBehaviour
     [SerializeField] private float chargeSpeed;
     [SerializeField] private float chargeCooldown;
     
-    private bool hasChargeAvailable;
     private Rigidbody2D _rb;
-    
+
+    private Vector3 enemyPositionOnGrid;
+    private Vector3 gridTarget;
+    private Vector3 originPos;
+    private Vector3 playerPositionOnGrid;
+    private Grid grid;
+    private float coolDown;
+    private float delta;
+    [SerializeField] private float animationTime = 1.0f;
+
+    enum ButterState
+    {
+        Searching,
+        Charging,
+        Recharging
+    }
+
+    private ButterState state;
     
     private void Start()
     {
-        hasChargeAvailable = false;
+        grid = GetComponentInParent<Grid>();
         _rb = GetComponent<Rigidbody2D>();
     }
 
     public void InitButter(GameObject t)
     {
         target = t;
-        hasChargeAvailable = true;
     }
 
   
     private void Update()
     {
-        if (hasChargeAvailable)
+        enemyPositionOnGrid = grid.WorldToCell(transform.position);
+        playerPositionOnGrid = grid.WorldToCell(target.transform.position);
+
+        if (state == ButterState.Searching)
         {
-            StartCoroutine(Charge());
+            if ((enemyPositionOnGrid - playerPositionOnGrid).magnitude <= 1)
+            {
+                state = ButterState.Charging;
+                gridTarget = GetTargetPos();
+                originPos = transform.position;
+                delta = 0;
+
+            }
         }
+
+        if (state == ButterState.Charging)
+        {
+            
+            var tValue = delta / animationTime;
+
+            if (tValue >= 1)
+            {
+                transform.position = gridTarget;
+                state = ButterState.Recharging;
+                coolDown = chargeCooldown;
+            }
+            
+            var distanceVec = gridTarget - originPos;
+            var deltaVec = tValue * tValue * tValue * distanceVec;
+            transform.position = originPos + deltaVec;
+            
+            delta += Time.deltaTime;
+        }
+
+        if (state == ButterState.Recharging)
+        {
+            if (coolDown <= 0)
+            {
+                state = ButterState.Searching;
+            }
+            coolDown -= Time.deltaTime;
+        }
+        
+        
     }
 
     private Vector2 FindPlayer()
@@ -43,27 +98,27 @@ public class ButterActions : MonoBehaviour
         return (target.transform.position - transform.position).normalized;
 
     }
-
-    private IEnumerator Charge()
-    {
-        hasChargeAvailable = false;
-        
-        var toPlayer = FindPlayer();
-        
-        yield return new WaitForSeconds(0.5f);
-        
-        _rb.AddForce(toPlayer * chargeSpeed, ForceMode2D.Impulse);
-        StartCoroutine(CoolDown());
-        yield return null;
-
-    }
-
-    private IEnumerator CoolDown()
-    {
-        yield return new WaitForSeconds(chargeCooldown);
-        hasChargeAvailable = true;
-
-    }
-
     
+    
+    private Vector3 GetTargetPos()
+    {
+        Vector3 difference = playerPositionOnGrid - enemyPositionOnGrid;
+
+        if (Mathf.Abs(difference.x) > 0.9f || Mathf.Abs(difference.y) > 0.9f)
+        {
+            Vector3 newPos = transform.position;
+            switch (Mathf.Abs(difference.x) >= Mathf.Abs(difference.y))
+            {
+                case true:
+                    _ = difference.x >= 0 ? newPos.x += 1 : newPos.x -= 1;
+                    break;
+                case false:
+                    _ = difference.y >= 0 ? newPos.y += 1 : newPos.y -= 1;
+                    break;
+            }
+            return newPos;
+        }
+
+        return transform.position;
+    }   
 }
