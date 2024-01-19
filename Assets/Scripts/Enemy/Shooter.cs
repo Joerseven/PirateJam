e847118040f0a67261c926bd2238c039f096bb7d
AdSand <149825252+AdSand@users.noreply.github.com>
2024-01-19 16:15:07 +0000

    Mayo now squeezes when shooting

--- Assets/Scripts/Enemy/Shooter.cs
+++ Assets/Scripts/Enemy/Shooter.cs
@@ -11,7 +11,16 @@ public class Shooter : MonoBehaviour, IEnemy
     [SerializeField] private float attackRange = 5.0f;
     [SerializeField] private float attackCooldown = 2.0f;
 
+    SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite unsqueezed;
+    [SerializeField] Sprite squeezed;
+
     private bool canAttack = true;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     public void Attack()
     {
@@ -42,7 +51,15 @@ public class Shooter : MonoBehaviour, IEnemy
         Vector2 targetDir = Player.Instance.transform.position - transform.position;
         GameObject newBullet = Instantiate(enemyProjectilePrefab, projectileSpawnPoint.position, Quaternion.identity);
         newBullet.transform.right = targetDir;
+        StartCoroutine(SqueezeBottle());
         yield return new WaitForSeconds(attackCooldown);
         canAttack = true;
+    }
+
+    private IEnumerator SqueezeBottle()
+    {
+        spriteRenderer.sprite = squeezed;
+        yield return new WaitForSeconds(1);
+        spriteRenderer.sprite = unsqueezed;
     }
 }
