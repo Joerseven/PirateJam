1efe7265dc990a9e0ec9ffbae96476b6f5ea54f0
Eddu <id26dabral@gmail.com>
2024-01-25 17:31:17 +0000

    im gonna geniunuely end myself
    kms :(

--- Assets/Scripts/Player/swipeAnimController.cs
+++ Assets/Scripts/Player/swipeAnimController.cs
@@ -7,43 +7,21 @@ public class swipeAnimController : MonoBehaviour
     [SerializeField]  private Animator _animator;
     [SerializeField] private AnimationClip[] swipeAnimations;
     
-    void Start()
-    {
-        // _animator = GetComponent<Animator>();
-        //
-        // if (_animator == null)
-        // {
-        //     Debug.LogError("No animator found");
-        // }
-    }
-    
 
     public void playSwing(Vector2 dir)
     {
-        Debug.Log("Gettiong from player" + dir);
+       
         
         if (dir.y == 0)
         {
-            if (dir.x < 0)
-            {
-                _animator.SetTrigger("hori-rev");
-            }
-            else
-            {
-                //play going left
-                _animator.SetTrigger("hori-slash");
-            }
+            //play going left
+            _animator.SetTrigger(dir.x < 0 ? "hori-rev" : "hori-slash");
         }
 
-        if (dir.x != 0) return;
-        switch (dir.y)
+        if (dir.x == 0)
         {
-            case < 0:
-                _animator.SetTrigger("verti-rev");
-                break;
-            case > 0:
-                _animator.SetTrigger("verti-slash");
-                break;
+            //play going left
+            _animator.SetTrigger(dir.y > 0 ? "verti-slash" : "verti-rev");
         }
     }
 }
